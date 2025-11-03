6e9423b7311c1ef1f4565e7647546d38761bad93
arsenii <arseniy.kunilovskiy@gmail.com>
2025-11-03 11:38:36 -0500

    FixedList is completed

--- src/main/java/com/pluralsight/FixedList.java
+++ src/main/java/com/pluralsight/FixedList.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
-public class FixedList {
+import java.util.List;
+
+public class FixedList<T> {
+    private List<T> items;
+    private int maxSize;
+
+    public FixedList(List<T> items, int maxSize) {
+        this.items = items;
+        this.maxSize = maxSize;
+    }
+
+    public <item> void add(T item){
+        items.add(item);
+        maxSize++;
+    }
+    public List<T> getItems(){
+        return items;
+    }
 }
